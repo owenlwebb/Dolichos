4f4fbdd0dd8a0aa225b4ba5fb0d4886fc2214abc
Owen Webb <owebb@umich.edu>
2020-01-20 15:29:29 -0500

    Implemented a basic Google Sheets setter and getter

---
+++ sheets.py
@@ -0,0 +1,82 @@
+"""Google Sheets API Wrapper
+
+   Author: Owen Webb
+   Date: 1/20/2020
+"""
+import os.path
+import pickle
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+class GoogleSheets(object):
+   def __init__(self, sheet_id, rw=True, fromfile=True):
+      """Initialize and authenticate Google Sheets API.
+
+         sheet_id : if fromfile=True name of file containing spreadsheetID,
+                     if fromfile=False sheet_id is string literal
+         rw : read/write access? False implies read-only
+      """
+      # load sheet_id and set scope
+      if fromfile:
+         with open(sheet_id, 'r') as fin:
+            self.sheet_id = fin.read().strip()
+      else:
+         self.sheet_id = sheet_id
+
+      # set scope
+      if rw=True:
+         self.scope = 'https://www.googleapis.com/auth/spreadsheets'
+      else:
+         self.scope = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+      # set file vars
+      self.token_file = 'sheets_token.pickle'
+      self.auth_file = 'sheets_auth.json'
+
+      self.authenticate()
+
+   def authenticate(self):
+      """Authenticate the Google Sheets API, most code here stolen from:
+      https://developers.google.com/resources/api-libraries/documentation/sheets/v4/python/latest/sheets_v4.spreadsheets.values.html"""
+      if os.path.exists(self.token_file):
+         with open(self.token_file, 'rb') as token:
+            creds = pickle.load(token)
+      # If there are no (valid) credentials available, let the user log in.
+      if not creds or not creds.valid:
+         if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+         else:
+            flow = InstalledAppFlow.from_client_secrets_file(self.auth_file, 
+                                                             self.scope)
+            creds = flow.run_local_server(port=0)
+         # Save the credentials for the next run
+         with open(self.token_file, 'wb') as token:
+            pickle.dump(creds, token)
+
+      # see documentation at: 
+      self.sheet = build('sheets', 'v4', credentials=creds).spreadsheets()
+
+   def get_cell(self, coords):
+      """Return value of cell belonging to the authenticated sheet."""
+      req = self.sheet.values().get(spreadsheetId=self.sheet_id, range=coords)
+      res = req.execute()
+      return res.get('values', [])[0][0]
+
+   def set_cell(self, coords, val):
+      """Set value of cell belonging to the authenticated sheet."""
+      body = { 'range' : coords, 'values' : [[val]] }
+      req = self.sheet.values().update(spreadsheetId=self.sheet_id, 
+                                       range=coords, 
+                                       body=body, 
+                                       valueInputOption='USER_ENTERED')
+      req.execute()
+
+
+if __name__ == '__main__':
+   # TESTING
+   gs = GoogleSheets('the_doc.identity')
+   gs.set_cell('Owen!D5', 'hi johnny')
+   print(gs.get_cell('Owen!D5'))
